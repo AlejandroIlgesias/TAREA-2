46cada6558eb4a7c9e7ef6aa42cf04879a173c5b
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2023-04-16 13:20:39 +0200

    actualizado

--- tarea/Tarea2.py
+++ tarea/Tarea2.py
@@ -1,19 +1,24 @@
 import csv
-
-
+from funciones import ordenar,añadir,clasificar
 
 fichero=open("calificaciones.csv",newline="")
 leer_fichero=csv.reader(fichero,delimiter=";")
-
 
 lista1=[]
 for x in leer_fichero:
     lista1.append(x)
 fichero.close()
 
+#DICCIONARIOS
+lista_ord=ordenar(lista1)
 
+#AÑADIMOS NOTA FINAL
 
+lista_2=añadir(lista_ord)
 
+#CLASIFICAMOS LA LISTA EN APROBADOS Y SUSPENSOS
+
+aprobados,suspensos=clasificar(lista_2)
 
 
 
